d3e754613a86c8fe051a65649383b6cd1f387cbf
Jeremy Potts <jeremy.potts7@gmail.com>
2021-06-11 15:12:18 -0400

    Changed calc border from navy to black as a test

--- src/styles/CalcWrapper.js
+++ src/styles/CalcWrapper.js
@@ -7,9 +7,10 @@ const CalcWrapper = styled.div`
   max-height: 50%;
   max-width: 50%;
   background-color: lightblue;
-  border: 5px double navy;
+  border: 5px double black;
   border-radius: 20px;
-  box-shadow: rgba(50, 50, 93, 0.25) 0px 30px 60px -12px, rgba(0, 0, 0, 0.3) 0px 18px 36px -18px;
+  box-shadow: rgba(50, 50, 93, 0.25) 0px 30px 60px -12px,
+    rgba(0, 0, 0, 0.3) 0px 18px 36px -18px;
   text-align: center;
   font-family: monospace, sans-serif;
 `;
