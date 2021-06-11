eaff4f056dd158750e4be47a958d9bdb9580fd6d
Jeremy Potts <jeremy.potts7@gmail.com>
2021-06-11 15:14:56 -0400

    Netlify deployment worked! Changing border back to navy for now

--- src/styles/CalcWrapper.js
+++ src/styles/CalcWrapper.js
@@ -7,7 +7,7 @@ const CalcWrapper = styled.div`
   max-height: 50%;
   max-width: 50%;
   background-color: lightblue;
-  border: 5px double black;
+  border: 5px double navy;
   border-radius: 20px;
   box-shadow: rgba(50, 50, 93, 0.25) 0px 30px 60px -12px,
     rgba(0, 0, 0, 0.3) 0px 18px 36px -18px;
